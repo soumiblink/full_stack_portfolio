217c36d9518c2d00635f7d0e80e88e1c0fed1c4c
soumiblink <soumiblink@gmail.com>
2026-02-08 22:13:58 +0530

    update

--- src/config/contact.ts
+++ src/config/contact.ts
@@ -1,6 +1,6 @@
 export const contactInfo = {
   email: "soumi4717@gmail.com",
-  phone: "+91 9876543210",
+  phone: "+91 7044650630",
   location: "West Bengal, India",
   github: "https://github.com/soumiblink",
   linkedin: "https://linkedin.com/in/soumi-ghosh-7129012a5?",
