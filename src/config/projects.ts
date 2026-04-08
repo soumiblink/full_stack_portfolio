65826b7e5e8ed97b2d1657a7c9a2e8a725b82aeb
soumiblink <soumiblink@gmail.com>
2026-04-08 22:30:05 +0530

    update in projects

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -7,13 +7,31 @@ export interface Project {
 }
 
 export const projectsData: Project[] = [
+
   {
-    title: "Project Management App",
-    description: "PRO - a project management app designed to help you manage your projects more efficiently.",
-    tech: ["React", "Node.js", "MongoDB", "Express.js", "Tailwind CSS", "Framer Motion", "Nodemailer", "JWT", "TypeScript", "Zustand", "Zod", ],
-    github: "https://github.com/soumiblink/Project_Management_App",
-    live: "https://github.com/soumiblink/Project_Management_App",
+    title: "WorkFlow Pro",
+    description: "WorkFlow Pro - WorkFlow Pro is a modern, full-featured employee management system built with React 18 and Vite. It features comprehensive set of tools for managing employees, viewing analytics, and generating reports.",
+    tech: ["React.js", "React Router", "MongoDB", "JWT", "TailwindCSS", "Axios", "Recharts" ],
+    github: "https://github.com/soumiblink/react.js_dashboard",
+    live: "https://react-js-dashboard-chi.vercel.app/",
   },
+
+  {
+    title: "FinSight",
+    description: "FinSight - FinSight is a system designed to manage personal financial data through a structured, role-aware REST API. The core focus of the project is API design, data modeling, and enforcing access control at the server level — not at the client",
+    tech: ["Python", "Django", "Django REST Framework", "SQLite", "React 18", "Tailwind CSS", "JWT"],
+    github: "https://github.com/soumiblink/finsight",
+    live: "https://finsight-brown.vercel.app/",
+  },
+
+  {
+    title: "FlowGraph AI",
+    description: "An LLM-powered business data query system with interactive graph visualization, built on top of SAP Order-to-Cash (O2C) data..",
+    tech: [" Groq (LLaMA 3.3)", " Streamlit", "SQLite", "Python"],
+    github: "https://github.com/soumiblink/flowgraph-ai",
+    live: "https://flowgraph-ai-adm2yzuqqkju2we9jrkpjf.streamlit.app//",
+  },
+  
   {
     title: "AI powered Applicant Tracking System",
     description: "AI powered applicant tracking system - An advanced ATS that leverages AI to streamline recruitment processes, enhance candidate matching, and improve hiring decisions.",
@@ -28,13 +46,7 @@ export const projectsData: Project[] = [
     github: "https://github.com/soumiblink/StudyBuddy",
     live: "https://github.com/soumiblink/StudyBuddy",
   },
-  {
-    title: "WorkFlow Pro",
-    description: "WorkFlow Pro - WorkFlow Pro is a modern, full-featured employee management system built with React 18 and Vite. It features comprehensive set of tools for managing employees, viewing analytics, and generating reports.",
-    tech: ["React.js", "React Router", "MongoDB", "JWT", "TailwindCSS", "Axios", "Recharts" ],
-    github: "https://github.com/soumiblink/react.js_dashboard",
-    live: "https://github.com/soumiblink/react.js_dashboard",
-  },
+  
   {
     title: "Full Stack Portfolio",
     description: "Full Stack Portfolio - A personal portfolio website showcasing my projects, skills, and experience, built with a modern tech stack to demonstrate my full-stack development capabilities.",
@@ -56,12 +68,13 @@ export const projectsData: Project[] = [
     github: "https://github.com/soumiblink/Crop-Recommendation-System-Using-ML",
     live: "https://github.com/soumiblink/Crop-Recommendation-System-Using-ML",
   },
+  
   {
-    title: "Save Smart",
-    description: "A modern full-stack bookmarking app that lets users save links and instantly generate AI-powered summaries using Jina AI. Built with React, TypeScript, and Supabase for blazing-fast performance and secure auth.",
-    tech: ["React", "TypeScript","Tailwind CSS","Jina AI API", "Supabase"],
-    github: "https://github.com/soumiblink/save_smart",
-    live: "https://github.com/soumiblink/save_smart",
+    title: "Project Management App",
+    description: "PRO - a project management app designed to help you manage your projects more efficiently.",
+    tech: ["React", "Node.js", "MongoDB", "Express.js", "Tailwind CSS", "Framer Motion", "Nodemailer", "JWT", "TypeScript", "Zustand", "Zod", ],
+    github: "https://github.com/soumiblink/Project_Management_App",
+    live: "https://github.com/soumiblink/Project_Management_App",
   },
 ];
 
